8451ef7d5ef7deb33d81df6fccb9f5c49ad02023
Hemanta <34749546+hempan9@users.noreply.github.com>
2022-02-19 16:46:36 -0600

    docker compose file added

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,11 @@
-FROM openjdk:11
-EXPOSE 8080
-ARG JAR_TARGET=target/*.jar
-COPY ${JAR_TARGET} role-based-docker-app.jar
-ENTRYPOINT ["java", "-jar", "/role-based-docker-app.jar"]
+#FROM adoptopenjdk:11-jre-hotspot
+#ARG JAR_FILE=target/role-based-docker-app.jar
+#COPY ${JAR_FILE} role-based-docker-app.jar
+#ENTRYPOINT ["java", "-jar", "role-based-docker-app.jar"]
+FROM maven:3.8.2-jdk-8
 
+WORKDIR .
+COPY . .
+RUN mvn clean install -Dmaven.test.skip=true
+
+CMD mvn spring-boot:run
--- src/main/java/com/hmp/role/RoleBasedAuthorizationApplication.java
+++ src/main/java/com/hmp/role/RoleBasedAuthorizationApplication.java
@@ -1,11 +1,15 @@
 package com.hmp.role;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RoleBasedAuthorizationApplication {
+	private static final Logger log = LogManager.getLogger(RoleBasedAuthorizationApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(RoleBasedAuthorizationApplication.class, args);
+		log.fatal("print date {}", System.currentTimeMillis());
 	}
 }
